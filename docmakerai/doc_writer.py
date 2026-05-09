5b017f8a3332d0e355d5dd157c8feae803b09d3c
Abbos1308 <abbosravshanov2008@gmail.com>
2026-05-09 22:07:15 +0500

    version chnagedd

--- docmakerai/doc_writer.py
+++ docmakerai/doc_writer.py
@@ -2,7 +2,7 @@ from .groq import call_groq
 from .prompts import system_prompt_writer as system_prompt
 
 def doc_writer(info,ai_token):
-    resp = call_groq([{"role": "user", "content": info}], system_prompt=system_prompt)
+    resp = call_groq([{"role": "user", "content": info}],API_KEY=ai_token, system_prompt=system_prompt)  
     return resp["choices"][0]["message"]["content"]
 
 
